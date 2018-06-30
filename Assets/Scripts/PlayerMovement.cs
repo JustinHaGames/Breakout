e0a9befbacfe4e83bb23fbd22ab533942ec9d081
Unknown <justinha22@gmail.com>
2018-06-30 17:25:16 -0400

    Basics of player movement are done. Must start ball physics before moving onto collisions.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//Connect the player's rigidbody to the code to apply the velocity
+	public Rigidbody2D rb; 
+
+	//A vector2 that will be applied to the rigidbody. We will be manipulating this vectors values 
+	//and apply them to the player
+	Vector2 vel; 
+
+	//The speed that the player will move left to right
+	public float moveVelX;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Identify if left is being input with either the left arrow key or the A key
+		bool left = Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+		//Identify if right is being input with either the right arrow key or the D key
+		bool right = Input.GetKey (KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+
+		//This code applies the velocity to the player when their respected keys are pressed
+		//If no keys are pressed, the velocity is 0 
+		if (left) {
+			vel.x = -moveVelX;
+		}
+
+		if (right) {
+			vel.x = moveVelX;
+		}
+
+		if (!left && !right) {
+			vel.x = 0; 
+		}
+
+		rb.MovePosition ((Vector2)transform.position + vel * Time.deltaTime);
+	}
+}
